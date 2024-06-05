62cde3f83a48934a3c6664ab2aba8f910b466649
Szymon Sztreja <91730827+szymonsztreja@users.noreply.github.com>
2024-06-05 10:19:47 +0200

    6 restructure project (#8)
    * Structure update with some ui
    
    * JS
    
    * Done updating price based on length of note
    
    * handled payment

--- flaskr/note.py
+++ flaskr/note.py
@@ -1,16 +1,11 @@
 from flask import (Flask, Blueprint, render_template, session, request, g, redirect, url_for, jsonify)
-from bitcoin.bitcoin_actions import (transaction_cost, get_address_info)
+from bitcoin.bitcoin_actions import (transaction_cost, get_address_info, get_utxo, get_balance,  generate_new_address, transaction_brutto,create_raw_transaction, sign_transaction, send_transaction)
 import requests
 import subprocess
 import json
 
 
 bp = Blueprint('note', __name__, url_prefix='/note')
-
-
-def get_balance():
-    return  json.loads(subprocess.check_output(["bitcoin-cli", "-testnet", "-rpcwallet=testwallet", "getbalances"]))
-
 
 @bp.route("/", methods=["GET", "POST"])
 def note():
@@ -28,8 +23,10 @@ def note():
             session.clear()
             user_id = note.encode().hex()
             session['user_id'] = user_id
+            session["amount_to_pay"] = amount_to_pay
+            session["address"] = address
+            session["note"] = note
             print(amount_to_pay)
-            print(address)
             return redirect(url_for('note.wait_for_payment'))
 
 
@@ -38,24 +35,46 @@ def note():
     elif request.method == 'GET':
         address_info = get_address_info()
         address = address_info["address"]
+        #address = generate_new_address()
 
     return render_template('note/note.html', address=address)
 
 
 
+@bp.route("/success", methods=["GET"])
+def success():
+    address = session["address"]
+    note = session["note"]
+    balance = get_balance(address)
+    balance_after_fee = balance - transaction_cost(note)
+    op_data_hex = note.encode().hex()
+    utxo_txid,  utxo_vout = get_utxo(address)
+    txhex = create_raw_transaction(utxo_txid, utxo_vout, op_data_hex, address, balance_after_fee)
+    signed_hex = sign_transaction(txhex)
+    send_hex = send_transaction(signed_hex)
+    return render_template('note/success.html', send_hex=send_hex) 
+  
+
 @bp.route("/tx_cost", methods=["POST"])
 def calculate_transaction_cost():
     jsonData = request.get_json()
     note = jsonData["note"]
-    # Call your Python function to calculate the transaction cost
-    tx_cost = transaction_cost(note)
+    tx_cost = transaction_brutto(note)
     return jsonify(tx_cost=tx_cost)
 
+
+@bp.route("/get_utxo", methods=["GET"])
+def check_payment():
+    address = session["address"]
+    total_recived = get_balance(address)
+    return jsonify(total_recived=total_recived)
 
 
 @bp.route("/waiting")
 def wait_for_payment():
-    return render_template("note/wait_for_payment.html")
+    amount_to_pay = session["amount_to_pay"]
+    address = session["address"]
+    return render_template("note/wait_for_payment.html",amount_to_pay=amount_to_pay, address=address)
 
 
 @bp.route('/quit_session')
